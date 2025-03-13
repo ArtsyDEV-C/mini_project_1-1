7388b847628cdc1ecefc3b73155368c344f23bac
Shreejith Kumar Das S <shreejithisdumb@gmail.com>
2025-03-13 17:45:50 +0530

    Implemented getWeatherMedia function and optimized weather background and video selection logic

--- public/script.js
+++ public/script.js
@@ -107,7 +107,7 @@ function formatTime(date) {
     const minutes = date.getMinutes();
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
-    hours = hours ? 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12; // the hour '0' should be '12'
     const strMinutes = minutes < 10 ? '0' + minutes : minutes;
     return `${hours}:${strMinutes} ${ampm}`;
 }
